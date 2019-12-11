c8bba87df5e6fc6a47379a971c25ebef67af2682
charlesmartinreed <charlesmartinreed@icloud.com>
2019-12-10 20:44:26 -0600

    Create ejs template for store page.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
+const fs = require("fs");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
@@ -24,6 +25,25 @@ DEFINE STATIC/PUBLIC FOLDER
 ===================
  */
 app.use(express.static("public"));
+
+/*
+===================
+ROUTES
+===================
+ */
+
+app.get("/store", (req, res) => {
+  fs.readFile("items.json", (err, data) => {
+    if (err) {
+      res.status(500).end();
+    } else {
+      // pass down the file variables to our template file
+      res.render("store.ejs", {
+        items: JSON.parse(data)
+      });
+    }
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server now runnning on port number ${port}`);
